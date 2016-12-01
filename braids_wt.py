862aca034be830bb15d63a26bf376ea1dc6006ad
Michael Hermes <michaelehermes@gmail.com>
2016-12-01 10:56:38 -0600

    Removed matplotlib example.

--- braids_wt.py
+++ braids_wt.py
@@ -202,52 +202,9 @@ def single_plot(series1, wave_name, suffix, target_dir):
     plt.close()
 
 
-def plot_wavemap():
-
-    """Modified version of example from matplotlib docs.
-    Get rid of this in lieu of bokeh example below?
-
-    """
-    fig = plt.figure(figsize=(8, 8))
-
-    # gridspec inside gridspec
-    outer_grid = gridspec.GridSpec(4, 4, wspace=0.0, hspace=0.0)
-
-    for i in range(16):
-        inner_grid = gridspec.GridSpecFromSubplotSpec(3, 3,
-                subplot_spec=outer_grid[i], wspace=0.0, hspace=0.0)
-        a, b = int(i/4)+1,i%4+1
-        for j, (c, d) in enumerate(product(range(1, 4), repeat=2)):
-            ax = plt.Subplot(fig, inner_grid[j])
-            ax.plot(*squiggle_xy(a, b, c, d))
-            ax.set_xticks([])
-            ax.set_yticks([])
-            fig.add_subplot(ax)
-
-    all_axes = fig.get_axes()
-
-    #show only the outside spines
-    for ax in all_axes:
-        for sp in ax.spines.values():
-            sp.set_visible(False)
-        if ax.is_first_row():
-            ax.spines['top'].set_visible(True)
-        if ax.is_last_row():
-            ax.spines['bottom'].set_visible(True)
-        if ax.is_first_col():
-            ax.spines['left'].set_visible(True)
-        if ax.is_last_col():
-            ax.spines['right'].set_visible(True)
-
-    plt.show()
-
-
-
-
-
 def main():
     #plot_interp_waves(piano, 20)
-    animate_all_waves()
+    #animate_all_waves()
 
 
 if __name__ == '__main__':
